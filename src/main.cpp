0bc202a38a3b1a085711d50c12bb70f59f8c96a9
Lucas Siqueira <lucas.asiq@gmail.com>
2025-01-14 11:20:29 +0200

    add haversine distance json input generator

---
+++ src/main.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <random>
+#include <cstdint>
+
+#define X 180
+#define Y 90 
+
+struct Point
+{
+    double x0;
+    double y0;
+    double x1;
+    double y1;
+};
+
+double GenerateRandomNumber();
+
+double GenerateRandomNumber(uint8_t bounds)
+{
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    std::uniform_real_distribution<double> dis(-bounds, bounds);
+    return dis(gen);
+}
+
+void GenerateJsonFile(std::string_view filename, std::vector<Point> points)
+{
+    std::ofstream file(filename.data());
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return;
+    }
+
+    file << "{\n";
+    file << "  \"points\": [\n";
+    for (size_t i = 0; i < points.size(); i++)
+    {
+        file << "    {\n";
+        file << "      \"x0\": " << points[i].x0 << ",\n";
+        file << "      \"y0\": " << points[i].y0 << ",\n";
+        file << "      \"x1\": " << points[i].x1 << ",\n";
+        file << "      \"y1\": " << points[i].y1 << "\n";
+        file << "    }";
+        if (i < points.size() - 1)
+        {
+            file << ",";
+        }
+        file << "\n";
+    }
+    file << "  ]\n";
+    file << "}\n";
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <num_points>" << std::endl;
+        return 1;
+    }
+    uint64_t num_points = std::stoull(argv[1]);
+    
+    auto points = std::vector<Point>();
+    points.reserve(num_points);
+    for(uint64_t i = 0; i < num_points; i++)
+    {
+        Point p;
+        p.x0 = GenerateRandomNumber(X);
+        p.y0 = GenerateRandomNumber(Y);
+        p.x1 = GenerateRandomNumber(X);
+        p.y1 = GenerateRandomNumber(Y);
+        points.emplace_back(p);
+        std::cout << "Point " << i << ": (" << p.x0 << ", " << p.y0 << ") -> (" << p.x1 << ", " << p.y1 << ")" << std::endl;
+
+    }
+
+    GenerateJsonFile("points.json", points);
+
+    return 0;
+}
